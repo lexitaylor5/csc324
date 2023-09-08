650cff7ef4b1b2565825995a94c71371fe6430dd
lexitaylor5 <142541017+lexitaylor5@users.noreply.github.com>
2023-09-07 23:13:28 -0400

    Update ch03_Lexi_Taylor.js

--- ch03_Lexi_Taylor.js
+++ ch03_Lexi_Taylor.js
@@ -26,13 +26,13 @@ function countBs(string) {
     return countChar(string, "B");
 }
 function countChar(string, char) {
-    let count = 0;
+    let counted = 0;
     for (let i = 0; i < string.length; i++) {
         if(string[i] == char) {
-            count ++;
+            counted ++;
         }
     }
-    return count;
+    return counted;
 }
 console.log(countBs("BBC"));
 console.log(countChar("kakkerlak", "k"));
