508bcae3b93ae2d6a405d9d773b949db3290be44
lexitaylor5 <142541017+lexitaylor5@users.noreply.github.com>
2023-09-07 23:45:54 -0400

    Update ch03_Lexi_Taylor.js

--- ch03_Lexi_Taylor.js
+++ ch03_Lexi_Taylor.js
@@ -11,11 +11,15 @@ console.log(Math.min(0, -10));
 
 // Recursion
 function isEven(number) {
-    if (number == 0) return true;
-    else if (number == 1) return false;
-    else if (number < 0) return isEven(-number);
-    else return isEven(number - 2);
-  }
+    if (number == 0)
+        return true;
+    else if (number == 1)
+        return false;
+    else if (number < 0)
+        return isEven(-number);
+    else
+        return isEven(number - 2);
+}
 
 console.log(isEven(50));
 console.log(isEven(75));
